158f679a5cf6960ba5b91a8b63aa946e6ee840bb
Arjunbbam <arjunbam.0987@gmail.com>
2025-02-12 23:58:14 +0530

    Commit 12/02/2025 [FEBONACHI SERIES]

---
+++ loop_program/Fibonachi.java
@@ -0,0 +1,15 @@
+class Fibonachi
+{
+	public static void main(String[] args) {
+		int sum = 0;
+		int temp = sum;
+		System.out.println((sum)+" ");
+		int t=0;
+		for(int i=0;i<10;i++)
+		{
+			sum +=temp;
+			System.out.print(" "+sum+" ");
+
+		}
+	}
+}
